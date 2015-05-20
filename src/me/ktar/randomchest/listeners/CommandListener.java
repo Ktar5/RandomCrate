f663f24ea67ba8edf38103aa7ac51fbf5bb728e7
Ktar5 <buildfresh@gmail.com>
2015-05-19 22:59:46 -0700

    Move to laptop

--- src/me/ktar/randomchest/listeners/CommandListener.java
+++ src/me/ktar/randomchest/listeners/CommandListener.java
@@ -14,8 +14,39 @@ public class CommandListener implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender instanceof Player){
             Player player = (Player) sender;
+            switch(args.length){
+                case 0:
+
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
         }
         return false;
     }
+
+    public void createChest(Player player){
+
+    }
+
+    public boolean removeChest(Player player){
+
+    }
+
+    public boolean forceUseChest(Player player){
+
+    }
+
+    public boolean getKeyForChest(Player player){
+
+    }
+
+    public boolean
 
 }
